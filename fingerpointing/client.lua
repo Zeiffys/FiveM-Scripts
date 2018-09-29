ef2db2cbe86dab7e04c2dbe50ec0d26566e2fb8a
freznva <freznva@ya.ru>
2018-09-29 15:16:13 +0400

    fingerpointing: some optimization

--- fingerpointing/client.lua
+++ fingerpointing/client.lua
@@ -7,18 +7,11 @@ local function IsAllOk()
 		(IsPedAPlayer(playerPed)) and
 		(IsPedOnFoot(playerPed)) and
 		(not IsEntityDead(playerPed)) and
-		(not IsPedDucking(playerPed)) and
 		(not IsPedFalling(playerPed)) and
-		(not IsPedFleeing(playerPed)) and
-		(not IsPedGettingIntoAVehicle(playerPed)) and
-		(not IsPedHangingOnToVehicle(playerPed)) and
 		(not IsPedInAnyVehicle(playerPed, true)) and
-		(not IsPedInCombat(playerPed)) and
 		(not IsPedInMeleeCombat(playerPed)) and
 		(not IsPedInParachuteFreeFall(playerPed)) and
-		(not IsPedJacking(playerPed)) and
 		(not IsPedJumping(playerPed)) and
-		(not IsPedJumpingOutOfVehicle(playerPed)) and
 		(not IsPedPerformingStealthKill(playerPed)) and
 		(not IsPedPlantingBomb(playerPed)) and
 		(not IsPedProne(playerPed)) and
@@ -26,21 +19,14 @@ local function IsAllOk()
 		(not IsPedShooting(playerPed)) and
 		(not IsPedSwimming(playerPed)) and
 		(not IsPedSwimmingUnderWater(playerPed)) and
-		(not IsPedTryingToEnterALockedVehicle(playerPed)) and
 		(not IsPedUsingAnyScenario(playerPed)) and
 		(not IsPedVaulting(playerPed)) and
-		(not IsPedWearingHelmet(playerPed)) and
 		(not IsPlayerFreeAiming(playerId)) and
-		(not IsPlayerRidingTrain(playerId)) and
 		(not IsControlPressed(2, 24)) and
 		(not IsControlPressed(2, 25))
 	)
 
 	return isAllOk
-end
-
-local function IsNetworkTaskMoveActive()
-	return N_0x921ce12c489c4c41(PlayerPedId())
 end
 
 local function IsPointingActive()
@@ -61,7 +47,6 @@ local function SetLocalPlayerPointing(toggle)
 		SetPedConfigFlag(playerPed, 36, true)
 	else
 		N_0xd01015c7316ae176(playerPed, "Stop")
-
 		SetPedConfigFlag(playerPed, 36, false)
 		ClearPedSecondaryTask(playerPed)
 	end
@@ -71,20 +56,22 @@ end
 
 Citizen.CreateThread(function()
 	while true do
-		Citizen.Wait(0)
+		local isAllOk = IsAllOk()
 		local isPressed = IsControlJustPressed(2, 29)
 
-		if IsAllOk() and not IsPointingActive() then
+		if isAllOk and not isPointingActive then
 			if isPressed then
 				SetLocalPlayerPointing(true)
 			end
-		elseif IsAllOk() and IsPointingActive() then
+		elseif isAllOk and isPointingActive then
 			if isPressed then
 				SetLocalPlayerPointing(false)
 			end
-		elseif not IsAllOk() and IsPointingActive() then
+		elseif not isAllOk and isPointingActive then
 			SetLocalPlayerPointing(false)
 		end
+
+		Citizen.Wait(GetFrameTime()*1000)
 	end
 end)
 
