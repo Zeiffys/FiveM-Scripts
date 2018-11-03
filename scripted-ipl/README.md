8f0a7a189e9bb83a643a3b47802679d4c941a645
speedium <speedium666@ya.ru>
2018-11-04 01:31:59 +0400

    scripted-ipl: initial

---
+++ scripted-ipl/README.md
@@ -0,0 +1,7 @@
+## scripted-ipl
+
+This resource contains almost all scripted IPL found me in the game files.<br>
+The list of IPL from DLC part (namely: apartments, offices, bunkers, import/export garages) i took from another resource (I was too lazy to learn the names of interiors. I just took and checked that everything was fine with them).
+
+In the plans:
+ * Find all interiors props.
---
+++ scripted-ipl/client.lua
@@ -0,0 +1,508 @@
+local scriptedipl = {
+	-- Pillnox Hill Tunnel Hole: 26.854, -619.074, 31.628
+	"v_tunnel_hole",
+	"dt1_03_gr_closed", -- hole fence cover
+
+	-- FIB
+	"atriumglmission", -- roof glass breakable
+	-- "atriumglstatic", -- roof glass static
+	-- "fbi_repair", -- after the mission with the explosion
+	"fiblobby",
+
+	-- Union Depository
+	-- "dt1_03_shutter", -- Shutter: -17.254, -687.387, 32.338
+	"finbank", -- -8.269, -655.330, 33.451
+
+	-- IAA
+	"dt1_05_fib2_mission", -- Decoration: 114.272, -619.299, 206.046
+
+	-- Billboard 1: 416.460, -928.624, 45.696
+	-- "dt1_17_newbill", -- new: meltdown
+	"dt1_17_oldbill", -- old: alco holla
+
+	-- Pillbox Building Lift: -187.991, -1020.713, -29.283
+	"dt1_21_prop_lift_on",
+
+	-- Pillbox Hill Medical Center: 290.688, -586.643, 43.183
+	"rc12b_default",
+	-- "rc12b_fixed",
+	-- "rc12b_hospitalinterior", -- destroyed
+
+	-- Simeon Car Showroom: -65.303, -1102.127, 26.150
+	"shr_int",
+
+	-- UFO's
+	"ufo", -- Zancudo: -2052, 3237, 1457
+	-- "ufo_eye", -- Hippie base: 2490.5, 3774.8, 2414
+	-- "ufo_lod", -- Chiliad: 501.53, 5593.86, 796.23
+
+	-- Billboards 2 - Cinema - Vinewood Center: 298.626, 195.588, 105.032
+	-- "hw1_02_newbill", -- new: meltdown
+	-- "hw1_emissive_newbill"
+	"hw1_02_oldbill", -- old: random posters
+	"hw1_emissive_oldbill",
+
+	-- Cinema - Vinewood Center: 298.626, 195.588, 105.032
+	-- "redcarpet",
+
+	-- Space Interior - Scene - No Collision: 41.64376, -779.9391, 838.4024
+	-- "spaceinterior",
+	-- "spacetable",
+
+	-- Cypress Flats Broken Wall: 891.914, -2363.271, 30.371
+	"des_smash2_startimap", -- (loaded by default by game, if not loaded alternative)
+	-- "des_smash2_endimap",
+
+	-- Lester Factory: 718.104, -977.538, 24.127
+	-- "id2_14_pre_no_int", -- fake shit
+	"burnt_switch_off", -- light in the windows
+	"id2_14_during1", -- with interior
+	-- "id2_14_during2", -- without interior
+	-- "id2_14_during_door", -- idk for what it
+	-- "id2_14_on_fire", -- factory on fire
+	-- "id2_14_post_no_int", -- after the fire
+
+	-- Elysian Island
+	-- "cargoship", -- Cargoship: -183.613, -2398.232, 6.001
+	"pcranecont", -- Crane: -53.698, -2415.790, 5.003
+	"po1_08_mapcrane", -- Crane: 304.991, -2973.977, 4.891
+	-- "sunk_ship_fire", -- Sunk Cargoship: -183.613, -2398.232, 6.001
+	-- "sunkcargoship",
+	"talklaugh_pipe", -- Pipe: -68.404, -2531.555, 5.152
+
+	-- Buccaneer Way Dock Crane: 888.910, -2920.785, 5.900
+	"dockcrane1",
+
+	-- Strawberry 
+	"carwash_with_spinners", -- Carwash Spinners: 22.124, -1391.97, 30.488
+	"chop_props", -- Chop props: -11.867, -1423.651, 30.688
+	"coroner_int_on", -- Morgue: 237.861, -1359.282, 39.530
+	"coronertrash", -- Morgue Trash: 234.113, -1355.891, 29.577
+
+	-- Strawberry Steal Cars: 495.321, -1334.821, 29.325
+	-- "prop_cheetah_covered",
+	-- "prop_entityxf_covered",
+	-- "prop_jb700_covered",
+	-- "prop_ztype_covered",
+
+	-- Strawberry Billboard 3: -348.473, -1322.221, 45.106
+	-- "sc1_01_newbill", -- new: meltdown
+	"sc1_01_oldbill", -- old: empty
+
+	-- Rancho/Little Bighorn Ave - Billboard 4: 422.874, -1918.178, 35.231
+	-- "sc1_14_newbill", -- new: meltdown
+	"sc1_14_oldbill", -- old: mollis
+
+	-- Rancho Motel Door: 22.372, -1896.747, 23.047
+	"sc1_30_keep_closed",
+
+	-- Richards Majestic Cutscene Trailer Door: -1113.893, -502.836, 35.882
+	"bh1_02_cutscenetrailer",
+
+	-- Rockford Hills Carpark Door (there is already a dynamic door): -878.035, -359.435, 36.252
+	-- "bh1_08_carparkdoor", -- static
+
+	-- Gwc and Golfing Society Joshhse House: -1107.564, 293.394, 64.716
+	-- "bh1_47_joshhse_burnt", -- burn state
+	-- "bh1_47_joshhse_firevfx", -- fire
+	"bh1_47_joshhse_unburnt", -- normal state
+
+	-- Life Invader Lobby: 1041.270, -228.569, 39.013
+	"facelobby",
+
+	-- iFruit Billboard 5: -1328.857, -265.153, 51.407
+	"fruitbb",
+
+	-- Rockford Hills Golf Flags: -1030.755, -83.658, 44.084
+	"golfflags",
+
+	-- Rockford Hills Vangelico Fine Jewelry: -632.293, -237.925, 38.068
+	"post_hiest_unload",
+
+	-- Rockford Hills Max Renda: -578.241, -278.905, 35.302
+	"refit_unload",
+
+	-- Rockford Hills Michael House: 
+	"solomon5noshow", -- Trash in garage: -807.252, 162.186, 71.541
+
+	-- Little Seoul Carwash: -699.687, -933.264, 19.013
+	"kt_carwash",
+
+	-- Maze Bank Arena: -250.988, -2030.396, 29.946
+	"sp1_10_real_interior",
+
+	-- Santa Monica Ferris Wheel: -1663.971, -1126.739, 30.706
+	"ferris_finale_anim",
+
+	-- Del Perro Billboard Graffiti (Rarrgh!): -1433.865, -450.068, 54.830
+	"sm_15_bldgraf1",
+
+	-- Dignity Party Yacht: -2028.631, -1036.372, 5.624
+	-- "smboat",
+	-- "smboat_lodlights"
+
+	-- La Puerta Building: -1093.386, -1653.051, 3.049
+	"scafstartimap", -- normal state (loaded by default by game, if not loaded alternative)
+	-- "scafendimap", -- broken state
+
+	-- Vespucci Beach Triathlon Stuff (huh?): -1300.314, -1706.297, 1.737
+	"triathlon2_vbprops",
+
+	-- Floyd House 
+	-- "vb_30_crimetape", -- Crime Tape: -1150.206, -1521.590, 10.853
+	"vb_30_emissive", -- light in the windows
+	-- "vb_30_murder", -- blood on window
+
+	-- Vespucci Beach Scenario Stuff (maybe can be switched on/off by itself)
+	"vb_34_scenario1", -- -1347.150, -1659.379, 1.345
+	"vb_35_scenario1", -- -1571.939, -1131.973, 2.153
+	"vb_35_scenario2", -- -1536.549, -1215.566, 0.910
+	"vb_36_scenario1", -- -2043.100, -552.900, 6.808
+	"vb_36_scenario2", -- -1833.788, -878.5287, 2.015
+	"vb_36_scenario3", -- -1838.607, -799.791, 5.381
+
+	-- Banham Canyon House: -3085.565, 340.152, 7.348
+	"ch1_02_open",
+	-- "ch1_02_closed",
+
+	-- Pacific Bluffs
+	-- "ch1_07_triaf03", -- Triathlon Stuff: -2307.899, 429.383, 174.466
+	-- "koriztempwalls", -- Koritze Walls: -2172.506, 247.563, 184.601
+	-- "mic3_chopper_debris", -- Downed chopper: -2234.921, 262.312, 173.669
+
+	-- Vinewood Hills 
+	"ch2_01_party", -- Small party: -937.825, 1724.790, 181.236
+	"chophillskennel", -- 3671 House - Chop props: 19.643, 537.221, 170.413
+
+	-- Vinewood Hills Martin Madrazo: -1045.188, 651.049, 141.794
+	"des_stilthouse_imapstart", -- normal (loaded by default by game)
+	-- "des_stilthouse_imapend", -- destroyed
+	-- "des_stilthouse_rebuild",
+
+	-- Vinewood Hills Scene
+	-- "fos_tvstage", -- Fame or Shame stuff: 687.215, 580.716, 129.460
+	-- "linvader", -- Life Invader: 687.215, 580.716, 129.460
+
+	-- El Burro Heights Props: 1451.047, -2606.492, 48.319
+	"ch3_13_radioprops",
+
+	-- Senora Way Billboard Graffiti (Obesity): 1838.830, 2409.073, 53.839
+	"ch3_rd2_bishopschickengraffiti",
+
+	-- El Burro Heights Tanker: 1718.357, -1626.000, 112.477
+	"tankercrash_grp1", -- normal
+	-- "tankercrash_grp2", -- destroyed
+	"tankerexp_grp0", -- normal
+	-- "tankerexp_grp1", -- destroyed
+	-- "tankerexp_grp2", -- destroyed
+	-- "tankerexp_grp3", -- destroyed
+
+	-- Paleto Bay Hotel: -170.980, 6428.499, 31.915
+	"bnkheist_apt_norm", -- normal state (loaded by default by game, if not loaded alternative)
+	-- "bnkheist_apt_dest", -- destroyed
+	-- "bnkheist_apt_dest_vfx", -- fire
+
+	-- Cassidy Creek: -470.565, 4426.448, 38.666
+	"canyonriver01", -- normal
+	-- "canyonriver01_traincrash", -- train crash
+	"railing_start", -- normal (loaded by default by game, if not loaded alternative)
+	-- "railing_end", -- train crash
+	
+
+	-- Raton Canyon River: -1679.184 4452.051 3.103
+	-- "canyonrvrdeep", -- great depth
+	"canyonrvrshallow", -- small depth
+
+	-- Paleto Bay Cluckin Bell: -70.997, 6266.013, 31.820
+	-- "cs1_02_cf_offmission1", -- shutter
+	"cs1_02_cf_onmission1",
+	"cs1_02_cf_onmission2",
+	"cs1_02_cf_onmission3",
+	"cs1_02_cf_onmission4",
+
+	-- Great Ocean Highway Paddle's: -1816.983, 4701.271, 57.001
+	"mg-flight school 5",
+
+	-- Humane Labs Tunnel Lattice: 3832.850, 3665.670, -23.000
+	"chemgrill_grp1",
+
+	-- Alamo Sea Crashed Cargo Plane: 315.758, 3983.858, 3.741
+	"crashed_cargoplane",
+
+	-- Seaview Rd Triathlon Stuff: 2384.707, 4279.050, 30.095
+	-- "cs2_06_triaf02",
+
+	-- O'Neil Farm: 2452.621, 4953.866, 44.946
+	"farm", -- for normal and burned
+	"farmint", -- normal
+	"farm_props",
+	"des_farmhs_startimap",
+	-- "farm_burnt", -- burned
+	-- "farm_burnt_props",
+	-- "des_farmhs_endimap",
+
+	-- San Chianski Mountain Range Crashed Plane: 2807.466, 4762.709, 46.32659
+	"plane_crash_trench",
+
+	-- Zancudo River: 86.815, 3191.649, 30.463
+	"cs3_05_water_grp1",
+	--"cs3_05_water_grp2", -- used with crashed trailer from trv1_trail_* ipl
+
+	-- Fort Zancudo Gate: -1593.999, 2805.471, 17.008
+	"cs3_07_mpgates",
+
+	-- Stab City LostMC Meth Trailer: 34.314, 3728.789, 39.596
+	"methtrailer_grp1", -- normal
+	-- "methtrailer_grp2", -- destroyed and burned
+	-- "methtrailer_grp3", -- destroyed
+
+	-- Stab City LostMC Trailers:
+	"trailerparka_grp1", -- normal (loaded by default by game, if not loaded alternative)
+	-- "trailerparka_grp2", -- destroyed
+	"trailerparkb_grp1", -- normal (loaded by default by game, if not loaded alternative)
+	-- "trailerparkb_grp2", -- destroyed
+	"trailerparkc_grp1", -- normal (loaded by default by game, if not loaded alternative)
+	-- "trailerparkc_grp2", -- destroyed
+	"trailerparkd_grp1", -- normal (loaded by default by game, if not loaded alternative)
+	-- "trailerparkd_grp2", -- destroyed
+	"trailerparke_grp1", -- normal (loaded by default by game, if not loaded alternative)
+	-- "trailerparke_grp2", -- destroyed
+
+	-- Zancudo River Trailer: -17.385, 3041.096, 40.946
+	"trv1_trail_start", -- normal
+	-- "trv1_trail_finish", -- destroyed
+	-- "trv1_trail_end",
+
+	-- Grand Senora Desert Airport Props: 1731.903, 3280.941, 41.087
+	"airfield",
+
+	-- Sandy Sores Marina Dr. Trash: 2017.381, 3829.293, 31.690
+	"cs4_04_trash",
+
+	-- Sandy Sores Triathlon Stuff
+	-- "cs4_04_triaf03", -- 1578.038, 3836.014, 30.238
+	-- "cs4_08_triaf02", -- 1758.185, 3898.041, 34.851
+
+	-- Grand Senora Race Track: 1976.971, 3109.734, 47.732
+	-- "racetrack01",
+
+	-- Trevor Trailer: 1981.679, 3815.385, 32.375
+	-- "trevorstrailer", -- normal
+	"trevorstrailertidy", -- clean
+	-- "trevorstrailertrash", -- trashed
+
+	-- Grand Senora Desert Maze Bank Billboard: 2719.432, 3183.851, 64.498
+	"cs5_04_mazebillboardgraffiti",
+
+	-- Devis Quartz: 2778.485, 2831.761, 40.719
+	"cs5_4_trains",
+
+	-- Grand Senora Desert RON Billboard: 2111.412, 3081.259, 54.985
+	"cs5_roads_ronoilgraffiti",
+
+
+
+	-- DLC's PART --
+
+
+
+	-- Apartments
+	-- "apa_v_mp_h_01_a", -- Modern 1 Apartment: -786.866, 315.764, 217.638
+	-- "apa_v_mp_h_01_c", -- Modern 2 Apartment: -786.956, 315.622, 187.913
+	-- "apa_v_mp_h_01_b", -- Modern 3 Apartment: -774.012, 342.042, 196.686
+	-- "apa_v_mp_h_02_a", -- Mody 1 Apartment: -787.074, 315.819, 217.638
+	-- "apa_v_mp_h_02_c", -- Mody 2 Apartment: -786.819, 315.563, 187.913
+	-- "apa_v_mp_h_02_b", -- Mody 3 Apartment: -774.138, 342.031, 196.686
+	-- "apa_v_mp_h_03_a", -- Vibrant 1 Apartment: -786.624, 315.617, 217.638
+	-- "apa_v_mp_h_03_c", -- Vibrant 2 Apartment: -786.958, 315.797, 187.913
+	-- "apa_v_mp_h_03_b", -- Vibrant 3 Apartment: -774.022, 342.171, 196.686
+	-- "apa_v_mp_h_04_a", -- Sharp 1 Apartment: -787.090, 315.703, 217.638
+	-- "apa_v_mp_h_04_c", -- Sharp 2 Apartment: -787.015, 315.707, 187.913
+	-- "apa_v_mp_h_04_b", -- Sharp 3 Apartment: -773.897, 342.152, 196.686
+	-- "apa_v_mp_h_05_a", -- Monochrome 1 Apartment: -786.988, 315.739, 217.638
+	-- "apa_v_mp_h_05_c", -- Monochrome 2 Apartment: -786.880, 315.663, 187.913
+	-- "apa_v_mp_h_05_b", -- Monochrome 3 Apartment: -774.067, 342.077, 196.686
+	-- "apa_v_mp_h_06_a", -- Seductive 1 Apartment: -787.142, 315.694, 217.638
+	-- "apa_v_mp_h_06_c", -- Seductive 2 Apartment: -787.096, 315.81, 187.913
+	-- "apa_v_mp_h_06_b", -- Seductive 3 Apartment: -773.955, 341.989, 196.686
+	-- "apa_v_mp_h_07_a", -- Regal 1 Apartment: -787.029, 315.711, 217.638
+	-- "apa_v_mp_h_07_c", -- Regal 2 Apartment: -787.057, 315.656, 187.913
+	-- "apa_v_mp_h_07_b", -- Regal 3 Apartment: -774.010, 342.096, 196.686
+	-- "apa_v_mp_h_08_a", -- Aqua 1 Apartment: -786.946, 315.565, 217.638
+	-- "apa_v_mp_h_08_c", -- Aqua 2 Apartment: -786.975, 315.723, 187.913
+	-- "apa_v_mp_h_08_b", -- Aqua 3 Apartment: -774.034, 342.029, 196.686
+
+	-- La-Mesa Autoshop Doors: 975.190, -1828.765, 31.184
+	"bkr_bi_id1_23_door",
+
+	-- East Vinewood The Lost MC House: 982.786, -106.492, 74.353
+	"bkr_bi_hw1_13_int",
+
+	-- Grand Senora Desert Hatches Terrain: 2052.478, 2949.142, 47.682
+	-- "xm_hatch_closed"
+	-- "xm_hatches_terrain",
+	-- "xm_bunkerentrance_door",
+	-- "xm_siloentranceclosed_x17",
+
+	-- Appartments & Offices
+	-- Arcadius Business Centre: -141.29, -621, 169
+	-- "ex_dt1_02_office_01a", -- Old Spice: Warm
+	-- "ex_dt1_02_office_01b", -- Old Spice: Classical
+	-- "ex_dt1_02_office_01c", -- Old Spice: Vintage
+	-- "ex_dt1_02_office_02a", -- Executive: Contrast
+	-- "ex_dt1_02_office_02b", -- Executive: Rich
+	-- "ex_dt1_02_office_02c", -- Executive: Cool
+	-- "ex_dt1_02_office_03a", -- Power Broker: Ice
+	-- "ex_dt1_02_office_03b", -- Power Broker: Conservative
+	-- "ex_dt1_02_office_03c", -- Power Broker: Polished
+	-- Maze Bank Building: -75.498, -827.189, 243.386
+	-- "ex_dt1_11_office_01a", -- Old Spice: Warm
+	-- "ex_dt1_11_office_01b", -- Old Spice: Classical
+	-- "ex_dt1_11_office_01c", -- Old Spice: Vintage
+	-- "ex_dt1_11_office_02b", -- Executive: Rich
+	-- "ex_dt1_11_office_02c", -- Executive: Cool
+	-- "ex_dt1_11_office_02a", -- Executive: Contrast
+	-- "ex_dt1_11_office_03a", -- Power Broker: Ice
+	-- "ex_dt1_11_office_03b", -- Power Broker: Conservative
+	-- "ex_dt1_11_office_03c", -- Power Broker: Polished
+	-- Lom Bank: -1579.756, -565.066, 108.523
+	-- "ex_sm_13_office_01a", -- Old Spice: Warm
+	-- "ex_sm_13_office_01b", -- Old Spice: Classical
+	-- "ex_sm_13_office_01c", -- Old Spice: Vintage
+	-- "ex_sm_13_office_02a", -- Executive: Contrast
+	-- "ex_sm_13_office_02b", -- Executive: Rich
+	-- "ex_sm_13_office_02c", -- Executive: Cool
+	-- "ex_sm_13_office_03a", -- Power Broker: Ice
+	-- "ex_sm_13_office_03b", -- Power Broker: Conservative
+	-- "ex_sm_13_office_03c", -- Power Broker: Polished
+	-- Maze Bank West: -1392.667, -480.474, 72.042
+	-- "ex_sm_15_office_01a", -- Old Spice: Warm
+	-- "ex_sm_15_office_01b", -- Old Spice: Classical
+	-- "ex_sm_15_office_01c", -- Old Spice: Vintage
+	-- "ex_sm_15_office_02b", -- Executive: Rich
+	-- "ex_sm_15_office_02c", -- Executive: Cool
+	-- "ex_sm_15_office_02a", -- Executive: Contrast
+	-- "ex_sm_15_office_03a", -- Power Broker: Ice
+	-- "ex_sm_15_office_03b", -- Power Broker: Convservative
+	-- "ex_sm_15_office_03c", -- Power Broker: Polished
+
+	-- Bunkers
+	-- "gr_case0_bunkerclosed", -- 848.6175, 2996.567, 45.81612
+	-- "gr_case1_bunkerclosed", -- 2126.785, 3335.04, 48.21422
+	-- "gr_case2_bunkerclosed", -- 2493.654, 3140.399, 51.28789
+	-- "gr_case3_bunkerclosed", -- 481.0465, 2995.135, 43.96672
+	-- "gr_case4_bunkerclosed", -- -391.3216, 4363.728, 58.65862
+	-- "gr_case5_bunkerclosed", -- 1823.961, 4708.14, 42.4991
+	-- "gr_case6_bunkerclosed", -- 1570.372, 2254.549, 78.89397
+	-- "gr_case7_bunkerclosed", -- -783.0755, 5934.686, 24.31475
+	-- "gr_case9_bunkerclosed", -- 24.43542, 2959.705, 58.35517
+	-- "gr_case10_bunkerclosed", -- -3058.714, 3329.19, 12.5844
+	-- "gr_case11_bunkerclosed", -- -3180.466, 1374.192, 19.9597
+
+	-- Heist Carrier: 3082.312 -4717.119 15.262
+	-- "hei_carrier",
+	-- "hei_carrier_int1",
+	-- "hei_carrier_int2",
+	-- "hei_carrier_int3",
+	-- "hei_carrier_int4",
+	-- "hei_carrier_int5",
+	-- "hei_carrier_int6",
+	-- "hei_carrier_lodlights",
+
+	-- Heist Yacht: -2032.751, -1035.182, 5.882
+	"hei_yacht_heist",
+	"hei_yacht_heist_bar",
+	"hei_yacht_heist_bedrm",
+	"hei_yacht_heist_bridge",
+	"hei_yacht_heist_enginrm",
+	"hei_yacht_heist_lounge",
+	"hei_yacht_heist_lodlights",
+
+	-- Import/Export Garages
+	-- "imp_dt1_02_modgarage",
+	-- "imp_dt1_02_cargarage_a",
+	-- "imp_dt1_02_cargarage_b",
+	-- "imp_dt1_02_cargarage_c",
+	-- "imp_dt1_11_modgarage",
+	-- "imp_dt1_11_cargarage_a",
+	-- "imp_dt1_11_cargarage_b",
+	-- "imp_dt1_11_cargarage_c",
+	-- "imp_sm_13_modgarage",
+	-- "imp_sm_13_cargarage_a",
+	-- "imp_sm_13_cargarage_b",
+	-- "imp_sm_13_cargarage_c",
+	-- "imp_sm_15_modgarage",
+	-- "imp_sm_15_cargarage_a",
+	-- "imp_sm_15_cargarage_b",
+	-- "imp_sm_15_cargarage_c",
+
+
+
+	-- ANOTHER IPL's --
+
+
+
+	-- I found some ipl which are fully controlled by the game. So I decided not to add them, as it is pointless.
+	-- I still added them for your interest...
+	-- "vb_29_day", -- -1257.658, -1453.584, 4.346
+	-- "vb_29_night",
+	-- "vb_30_day", -- -1200.461, -1542.268, 4.369
+	-- "vb_30_night_shutter",
+	-- "vb_33_dayprops", -- -1121.538, -1647.396, 7.461
+	-- "vb_33_night",
+	-- "vb_34_day", -- -1413.454, -1473.928, 2.253
+	-- "vb_34_evening",
+	-- "vb_34_morning",
+	-- "vb_34_night",
+	-- "vb_35_day", -- -1526.040, -1240.362, 1.252
+	-- "vb_35_evening",
+	-- "vb_35_morning",
+	-- "vb_35_night",
+	-- "vb_36_day", -- -1781.592, -898.022, 5.467
+	-- "vb_36_evening",
+	-- "vb_36_morning",
+
+	-- "ch1_01_day", -- -2956.973, 20.126, 10.607
+	-- "ch1_01_evening", -- -2930.466, -12.246, 5.349
+	-- "ch1_01_morning", -- -2953.982, -15.976, 4.505
+	-- "ch1_01_night", -- -2944.808, -6.961, 5.605
+	-- "ch1_01_props_night", -- -2768.700, -62.59999, 5.058
+	-- "ch1_12_props_day", -- -3272.289, 1130.583, 1.468
+	-- "ch1_12_props_night", -- -3226.935, 1234.811, 2.834
+}
+
+Citizen.CreateThread(function()
+	for k, v in pairs(scriptedipl) do
+		if IsIplActive(v) then
+			Citizen.Trace(" *scripted-ipl: "..v.." already loaded.\n")
+		else
+			RequestIpl(v)
+			Citizen.Wait(0)
+			if IsIplActive(v) then
+				Citizen.Trace(" *scripted-ipl: "..v.." loaded.\n")
+			else
+				Citizen.Trace(" *scripted-ipl: "..v.." error while loading.\n")
+			end
+		end
+	end
+end)
+
+--[[
+AddEventHandler("onResourceStop", function(resource)
+	if resource == GetCurrentResourceName() then
+		for k, v in pairs(scriptedipl) do
+			if IsIplActive(v) then
+				RemoveIpl(v)
+				Citizen.Wait(0)
+				if IsIplActive(v) then
+					Citizen.Trace(" *scripted-ipl: "..v.." error while unloading.\n")
+				else
+					Citizen.Trace(" *scripted-ipl: "..v.." unloaded.\n")
+				end
+			else
+				Citizen.Trace(" *scripted-ipl: "..v.." already unloaded.\n")
+			end
+		end
+	end
+end)
+]]
